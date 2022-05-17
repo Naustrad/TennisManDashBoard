f4b79cdd84645135bef29d0af2cc855ff07d09d3
Naustrad <ali.naustrad@gmail.com>
2022-05-17 20:30:20 +0100

    env example

--- tennisman-dashboard.back-end/src/main.ts
+++ tennisman-dashboard.back-end/src/main.ts
@@ -1,7 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { initializeApp } from "firebase/app";
 async function bootstrap() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyAlLAradOLLHp0jTgkaR8cuPOfTUn2U93s",
+    authDomain: "tennisstats-25659.firebaseapp.com",
+    projectId: "tennisstats-25659",
+    storageBucket: "tennisstats-25659.appspot.com",
+    messagingSenderId: "727142983569",
+    appId: "1:727142983569:web:944a7a4f5f1d8223fcdc30"
+  };
+  const firebase = initializeApp(firebaseConfig);
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(3000);
