e1d037f1fe4f9a7a27ec9e8193ed23b702ede5a4
AliGarouachi <36104357+AliGarouachi@users.noreply.github.com>
2022-05-18 14:39:20 +0100

    Update main.ts

--- tennisman-dashboard.back-end/src/main.ts
+++ tennisman-dashboard.back-end/src/main.ts
@@ -1,16 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { initializeApp } from "firebase/app";
 async function bootstrap() {
-  const firebaseConfig = {
-    apiKey: "AIzaSyAlLAradOLLHp0jTgkaR8cuPOfTUn2U93s",
-    authDomain: "tennisstats-25659.firebaseapp.com",
-    projectId: "tennisstats-25659",
-    storageBucket: "tennisstats-25659.appspot.com",
-    messagingSenderId: "727142983569",
-    appId: "1:727142983569:web:944a7a4f5f1d8223fcdc30"
-  };
-  const firebase = initializeApp(firebaseConfig);
   const app = await NestFactory.create(AppModule);
   //A ne pas reproduire chez sois on evite de enable le cors pour plus de securité
   //J'ai fais ça pour facilité les testes
